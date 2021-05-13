e63fea9e624e8a865fdd9e75d6d83d33b485705e
sage-cau <dan06052@naver.com>
2021-05-13 13:24:17 +0900

    5월 MT 포스터 팝업창 구현

---
+++ js/popup.js
@@ -0,0 +1,42 @@
+(()=>{
+    let popup = document.querySelector(".popup");
+    let close_btn = document.querySelector(".close-btn");
+    let popup_content = document.querySelector(".popup-content");
+    let popup_img = popup_content.querySelector(".popup-img");
+    let link_btn_container = popup_content.querySelector(".link-btn-container");
+    let links = popup_content.querySelectorAll("a");
+    
+    
+
+    window.addEventListener('load', () => {
+        close_btn.style.bottom = `${popup_content.offsetHeight + 5}px`;
+    });
+
+    
+    window.addEventListener('resize', () => {
+        close_btn.style.bottom = `${popup_content.offsetHeight + 5}px`;
+    });
+
+    close_btn.onclick = () => {
+        popup.style.display = "none";
+    }
+
+    let today = new Date();
+    let start_day = new Date('2021-05-21 00:00:00 GMT+0900');
+    let end_day = new Date('2021-05-22 23:59:59 GMT+0900');
+    if(+today >= +start_day && +today <= +end_day) {
+        links[0].href = "https://us02web.zoom.us/j/7990705699?pwd=TVZpTVRJUTBNdklEczMrUXMzeUZlUT09";
+        links[1].href = "https://www.youtube.com/channel/UCW5f_GaYz2-VFkSBOFfL6Sg/videos";
+
+        popup_img.src = "images/2021_05MT.jpg";
+        popup_img.style.paddingBottom = `${link_btn_container.offsetHeight - 2}px`
+        link_btn_container.style.opacity = 1;
+
+    }
+    else if(today < start_day) {
+        popup_img.src = "images/2021_05MT_square.jpg";
+    }
+    else {
+        popup.style.display = "none";
+    }
+})();
